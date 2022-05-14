42c0c321cb0f91a53eb23823004da806cc1ff071
alexzhang1030 <1642114555@qq.com>
2022-05-14 14:42:01 +0800

    chore: migrate to monorepo

---
+++ playground/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div class="w-100"></div>
+  <div class="w-200"></div>
+</template>
+<style scoped>
+</style>
--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {miniunocss} from './miniunocss/index'
+import { miniunocss } from '@developer-plus/mini-unocss-core'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), miniunocss({
+  plugins: [vue(), , miniunocss({
     presets: [
       {
         rules: [
