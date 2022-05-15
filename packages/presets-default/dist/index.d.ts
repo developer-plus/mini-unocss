c78a836ffcfd8d60b5d7ca5374c18af25f64a938
Hongbusi <q951686662@qq.com>
2022-05-15 10:38:45 +0800

    chore: update .gitignore

--- packages/presets-default/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare type PresetsRules = PresetsRulesString | PresetsRulesReg;
-declare type PresetsRulesString = [string, (isNotUnit: boolean) => any];
-declare type PresetsRulesReg = [RegExp, (exec: RegExpExecArray, isNotUnit: boolean) => any];
-
-declare const _default: {
-    rules: PresetsRules[];
-}[];
-
-export { _default as default };
