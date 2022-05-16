1b39a11d1e64677faaca7d6cdc3934c3fc4ae5a4
Enoch <qhxngzx@gmail.com>
2022-05-16 19:07:11 +0800

    fix: fix issue #15

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { miniunocss } from '@developer-plus/mini-unocss-core'
-import presets from '@developer-plus/mini-unocss-presets-default'
+import { miniunocss } from '@developer-plus/mini-unocss'
+import presets from '@developer-plus/mini-unocss-preset'
 
 // https://vitejs.dev/config/
 export default defineConfig({
